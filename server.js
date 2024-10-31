fa0f73464517c56874784837530213cef64aaad6
Flo0108 <florian.zeif1@gmail.com>
2024-10-31 10:45:11 +0100

    Update server.js to use Render's PORT

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -7,10 +6,13 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Serve static files from the public directory
 app.use(express.static(__dirname + '/public'));
 
+// Store connected players
 let players = {};
 
+// Handle socket connections
 io.on('connection', (socket) => {
     console.log('New player connected:', socket.id);
 
@@ -39,6 +41,8 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+// Use PORT environment variable or default to 3000
+const PORT = process.env.PORT || 3000; 
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
