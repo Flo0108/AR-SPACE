629954c76caaf2d8265e9d193d2beeac8cd8bd3e
Flo0108 <florian.zeif1@gmail.com>
2024-10-31 11:28:09 +0100

    Player Movement fix

--- server.js
+++ server.js
@@ -16,19 +16,19 @@ io.on('connection', (socket) => {
     console.log('New player connected:', socket.id);
 
     // Add new player to the list
-    players[socket.id] = { x: 0, y: 1.6, z: 0 };  // initial position
+    players[socket.id] = { position: { x: 0, y: 1.6, z: 0 } };  // initial position
 
     // Send current players to the new player
     socket.emit('currentPlayers', players);
 
     // Notify others of new player
-    socket.broadcast.emit('newPlayer', { id: socket.id, position: players[socket.id] });
+    socket.broadcast.emit('newPlayer', { id: socket.id, position: players[socket.id].position });
 
     // Handle movement updates
     socket.on('move', (data) => {
         if (players[socket.id]) {
-            players[socket.id] = data;
-            socket.broadcast.emit('move', { id: socket.id, position: data });
+            players[socket.id].position = data.position; // Update position on server
+            socket.broadcast.emit('move', { id: socket.id, position: data.position }); // Broadcast to others
         }
     });
 
